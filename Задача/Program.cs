cd2a30663c5b5dc05eefd09633d44cb0cbde4568
master <yarasmoothbeat@gmail.com>
2023-11-23 14:00:23 +0500

    Программа

---
+++ Задача/Program.cs
@@ -0,0 +1,31 @@
+﻿string[] ChangeStringArray (string[] args)
+{
+    string[] newStringArray = new string[args.Length];
+    for(int i = 0; i < args.Length; i++)
+    {
+        string str = args[i];
+        if (str.Length <= 3)
+        {
+            newStringArray[i] = str;
+        } 
+    }
+    return newStringArray;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write( array[i] + " " );
+    }
+}
+
+string[] strings = new string[4];
+strings[0] = "Hello";
+strings[1] = "2";
+strings[2] = "world";
+strings[3] = ":-)";
+
+PrintArray(strings);
+Console.WriteLine();
+PrintArray(ChangeStringArray(strings));
